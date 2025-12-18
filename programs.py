9f3a2c1ef590af82e77dc8e0480f8e3a8ea4a788
Madhan <mmadhanjm@gmail.com>
2025-12-18 15:58:10 +0530

    Initial commit

---
+++ programs.py
@@ -0,0 +1,14 @@
+template = "Hello <<UserName>>, How are you?"
+
+while True:
+    username=input("Enter Your Name : ").strip()
+    if(len(username) >=3):
+        break
+    else:
+        print("Username Must Have At Least 3 Characters")
+
+result=template.replace("<<UserName>>",username)  
+print(result)  
+
+
+    
